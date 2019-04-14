1113feb0a45d34252dcd26f61efc24d79813ea54
StepanSS <stanstepan84@gmail.com>
2019-04-14 23:22:04 +0300

    wavesurf-annotation

---
+++ wavesurfer-annotation/main.js
@@ -0,0 +1,176 @@
+/**
+ * Create a WaveSurfer instance.
+ */
+var wavesurfer;
+// Create an instance of wave surfer with its configuration
+document.addEventListener('DOMContentLoaded', function() {
+    wavesurfer = WaveSurfer.create({
+        container: '#waveform',
+        height: 100,
+        pixelRatio: 1,
+        scrollParent: true,
+        normalize: true,
+        minimap: true,
+        backend: 'MediaElement',
+        plugins: [
+            WaveSurfer.regions.create(),
+            WaveSurfer.minimap.create({
+                height: 30,
+                waveColor: '#ddd',
+                progressColor: '#999',
+                cursorColor: '#999'
+            }),
+            WaveSurfer.timeline.create({
+                container: '#wave-timeline'
+            })
+        ]
+    });
+   // Load the audio file from your own domain !
+    wavesurfer.load('demo.mp3');
+    
+    wavesurfer.on("ready", function(){
+        // Do something when the file has been loaded
+        buttons.play.disabled = false;
+        // Do whatever you need to do with the player
+        wavesurfer.play();
+        wavesurfer.pause();
+        wavesurfer.stop();
+    });
+    // Store the 3 buttons in some object
+            var buttons = {
+                play: document.getElementById("play"),
+                pause: document.getElementById("pause"),
+                stop: document.getElementById("stop")
+            };
+    
+    // Handle Play button
+            buttons.play.addEventListener("click", function(){
+                wavesurfer.play();
+
+                // Enable/Disable respectively buttons
+                buttons.stop.disabled = false;
+                buttons.pause.disabled = false;
+                buttons.play.disabled = true;
+            }, false);
+    // Handle Pause button
+            buttons.pause.addEventListener("click", function(){
+                wavesurfer.pause();
+
+                // Enable/Disable respectively buttons
+                buttons.pause.disabled = true;
+                buttons.play.disabled = false;
+            }, false);
+    // Handle Stop button
+            buttons.stop.addEventListener("click", function(){
+                wavesurfer.stop();
+
+                // Enable/Disable respectively buttons
+                buttons.pause.disabled = true;
+                buttons.play.disabled = false;
+                buttons.stop.disabled = true;
+            }, false);
+    
+    /* Regions */
+
+    wavesurfer.on('ready', function() {
+        wavesurfer.enableDragSelection({
+            color: randomColor(0.1)
+        });
+
+        if (localStorage.regions) {
+            loadRegions(JSON.parse(localStorage.regions));
+        } 
+        else {
+            // loadRegions(
+            //     extractRegions(
+            //         wavesurfer.backend.getPeaks(512),
+            //         wavesurfer.getDuration()
+            //     )
+            // );
+            wavesurfer.util
+                .ajax({
+                    responseType: 'json',
+                    url: 'annotations.json'
+                })
+                .on('success', function(data) {
+                    loadRegions(data);
+                    saveRegions();
+                });
+        }
+    });
+    wavesurfer.on('region-click', function(region, e) {
+        e.stopPropagation();
+        // Play on click, loop on shift click
+        e.shiftKey ? region.playLoop() : region.play();
+    });
+//    wavesurfer.on('region-click', editAnnotation);
+//    wavesurfer.on('region-updated', saveRegions);
+//    wavesurfer.on('region-removed', saveRegions);
+    wavesurfer.on('region-in', showNote);
+
+    wavesurfer.on('region-play', function(region) {
+        region.once('out', function() {
+            wavesurfer.play(region.start);
+            wavesurfer.pause();
+        });
+    });
+
+//    /* Toggle play/pause buttons. */
+//    var playButton = document.querySelector('#play');
+//    var pauseButton = document.querySelector('#pause');
+//    wavesurfer.on('play', function() {
+//        playButton.style.display = 'none';
+//        pauseButton.style.display = '';
+//    });
+//    wavesurfer.on('pause', function() {
+//        playButton.style.display = '';
+//        pauseButton.style.display = 'none';
+//    });
+    
+    /**
+     * Random RGBA color.
+     */
+    function randomColor(alpha) {
+        return (
+            'rgba(' +
+            [
+                ~~(Math.random() * 255),
+                ~~(Math.random() * 255),
+                ~~(Math.random() * 255),
+                alpha || 1
+            ] +
+            ')'
+        );
+    }
+    
+    /**
+     * Load regions from localStorage.
+     */
+    function loadRegions(regions) {
+        let regLen = regions.length
+        for(let i = 0; i<regLen; i++){
+//            console.log(regions[i].start);
+            regions[i].color =randomColor(0.1); 
+            if(i<regLen-1){
+                regions[i].end=regions[i+1].start;
+
+            }
+            wavesurfer.addRegion(regions[i]);
+        }
+//        regions.forEach(function(region) {
+//            region.color = randomColor(0.1);
+//            wavesurfer.addRegion(region);
+//        });
+    }
+    /**
+     * Display annotation.
+     */
+    function showNote(region) {
+        if (!showNote.el) {
+            showNote.el = document.querySelector('#subtitle');
+        }
+        showNote.el.textContent = region.data.note || '–';
+    }
+
+    
+});
