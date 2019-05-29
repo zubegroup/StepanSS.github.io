a622d2a8fa33cabd426b55a4edd2ed20828896d6
StepanSS <stanstepan84@gmail.com>
2019-05-30 01:03:21 +0300

    remove extra lines

--- siteSample/settings-page/magnific-popup/main.js
+++ siteSample/settings-page/magnific-popup/main.js
@@ -18,35 +18,5 @@ $(document).ready(function() {
 		}
 	});
 });
-$(document).ready(function() {
-	$('.form-group').magnificPopup({
-		delegate: 'a',
-//		type: 'img',
-//		tLoading: 'Loading image #%curr%...',
-//		mainClass: 'mfp-img-mobile',
-		gallery: {
-			enabled: true,
-//			navigateByImgClick: true,
-			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-		},
-		image: {
-			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-			titleSrc: function(item) {
-				return item.el.attr('title') + '<small>Select</small>';
-			}
-		}
-	});
-});
 
-// From an element with ID #popup
-//$('.form-group').magnificPopup({
-//  items: {
-//      src: '.form-check',
-//      type: 'inline'
-//  }
-//});
 
-//$('.open-popup-link').magnificPopup({
-//  type:'inline',
-//  midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
-//});
